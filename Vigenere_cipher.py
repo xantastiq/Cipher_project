1b9ea66b0ad8f506c6ef1588b140371f24fd8c09
xantastiq <teshraq@gmail.com>
2025-06-06 01:20:04 +0600

    Update and rename cipher.py to Vigenere_cipher.py

--- Vigenere_cipher.py
+++ Vigenere_cipher.py
@@ -1,5 +1,4 @@
-text = 'mrttaqrhknsw ih puggrur'
-custom_key = 'python'
+# vigenere_cipher.py
 
 def vigenere(message, key, direction=1):
     key_index = 0
@@ -7,30 +6,30 @@ def vigenere(message, key, direction=1):
     final_message = ''
 
     for char in message.lower():
-
-        # Append any non-letter character to the message
         if not char.isalpha():
             final_message += char
-        else:        
-            # Find the right key character to encode/decode
+        else:
             key_char = key[key_index % len(key)]
             key_index += 1
 
-            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset*direction) % len(alphabet)
+            new_index = (index + offset * direction) % len(alphabet)
             final_message += alphabet[new_index]
-    
+
     return final_message
 
 def encrypt(message, key):
     return vigenere(message, key)
-    
+
 def decrypt(message, key):
     return vigenere(message, key, -1)
 
-print(f'\nEncrypted text: {text}')
-print(f'Key: {custom_key}')
-decryption = decrypt(text, custom_key)
-print(f'\nDecrypted text: {decryption}\n')
+if __name__ == "__main__":
+    text = 'mrttaqrhknsw ih puggrur'
+    custom_key = 'happycoding'
+
+    print(f'\nEncrypted text: {text}')
+    print(f'Key: {custom_key}')
+    decrypted = decrypt(text, custom_key)
+    print(f'\nDecrypted text: {decrypted}\n')
